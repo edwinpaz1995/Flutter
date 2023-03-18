2cdcc1b7b726ed71061d931aec901188d6c467e5
edwinpaz1995 <laureanopaz@hotmail.com>
2023-03-18 17:45:55 -0600

    Carga Inicial
    source inicial

---
+++ Flutter/p2/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Principal());
+}
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
